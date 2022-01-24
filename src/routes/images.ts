338f6966af320db2fc5f5d2a72d3a350534a8d3c
OmarMo7 <omar7.first@gmail.com>
2022-01-24 11:17:19 +0200

    Separating the route to another file

---
+++ src/routes/images.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import { query, validationResult } from 'express-validator';
+import handleRequest from '../utils/imagesUtils/handleGET';
+
+const router = express.Router();
+
+router.get('/images',
+  query('filename').exists({checkFalsy: true}).withMessage('This field cannot be empty!'),
+  query('width').exists({checkFalsy: true}),
+  query('height').exists({checkFalsy: true})
+  , (req: express.Request,res: express.Response)=>{
+
+    const errors = validationResult(req);
+
+    const fileName = req.query.filename as string;
+    const width = parseInt(req.query.width as string);
+    const height = parseInt(req.query.height as string);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    handleRequest(res, fileName,width,height);
+  });
+
+export default router;
