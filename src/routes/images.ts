95cee4990d872a3b02e288215dc0cc3f7fef6f2f
OmarMo7 <omar7.first@gmail.com>
2022-01-28 00:56:30 +0200

    adding type of any to the return of the GET route callback function.

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 import { query, validationResult } from 'express-validator';
-import handleRequest from '../utils/imagesUtils/handleGET';
+import {handleRequest} from '../utils/imagesUtils/handleGET';
 
 const router = express.Router();
 
-router.get('/images',
-  query('filename').exists({checkFalsy: true}).withMessage('This field cannot be empty!'),
-  query('width').exists({checkFalsy: true}),
-  query('height').exists({checkFalsy: true})
-  , (req: express.Request,res: express.Response)=>{
-
+router.get(
+  '/images',
+  query('filename').exists({ checkFalsy: true }).withMessage('This field cannot be empty!'),
+  query('width').exists({ checkFalsy: true }),
+  query('height').exists({ checkFalsy: true }),
+  (req: express.Request, res: express.Response): any => {
     const errors = validationResult(req);
 
     const fileName = req.query.filename as string;
@@ -20,7 +20,8 @@ router.get('/images',
       return res.status(400).json({ errors: errors.array() });
     }
 
-    handleRequest(res, fileName,width,height);
-  });
+    handleRequest(res, fileName, width, height);
+  },
+);
 
 export default router;
