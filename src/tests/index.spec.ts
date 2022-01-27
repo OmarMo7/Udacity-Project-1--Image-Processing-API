b048f70bf6a1b403f8f4a8af7eff2da2cc07f769
OmarMo7 <omar7.first@gmail.com>
2022-01-28 00:56:53 +0200

    adding a test block for testing the image resizing operation.

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,27 +1,37 @@
 import supertest from 'supertest';
-import {app} from '../index';
-
+import { app } from '../index';
+import { resizeImage } from '../utils/imagesUtils/handleGET';
 const request = supertest(app);
 
-describe('Test different endpoint responses', ()=>{
-  it('should get the endpoint successfully', async()=>{
+
+describe('Test image processing operation', () => {
+  it('the resultant image should have the same width and height as intended ', async () => {
+    const fileName = 'icelandwaterfall';
+    const width = 200; // intended width
+    const height = 300; // intended height
+    const resultantImage = await resizeImage(fileName,width,height);
+
+    expect(resultantImage.width).toEqual(width);
+    expect(resultantImage.height).toEqual(height);
+  });
+});
+
+
+describe('Test different endpoint responses', () => {
+  beforeAll(function() {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+    });
+  it('should get the endpoint successfully', async () => {
     const fileName = 'icelandwaterfall';
     const width = '200';
     const height = '300';
     const response = await request.get(`/api/images?filename=${fileName}&width=${width}&height=${height}`);
-    console.log(response);
     expect(response.status).toBe(200);
   });
 
-  it('gets an error with 400 (Bad Request)', async()=>{
-    const fileName2 = 'BlaBla';
-    const width2 = '400';
-    const height2 = '700';
-    const response = await request.get(`/api/images?filename=${fileName2}&width=${width2}&height=${height2}`);
-    expect(response.status).toBe(404);
-  });
+  //    
 
-  it('should come up with -express validator- middleware error', async()=>{
+  it('should come up with -express validator- middleware error', async () => {
     const fileName3 = '';
     const width3 = '400';
     const height3 = '700';
