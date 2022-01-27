b74325cf2a760687c958a54589d1f17913b59710
OmarMo7 <omar7.first@gmail.com>
2022-01-28 00:57:59 +0200

    adding return type for each function

--- src/utils/imagesUtils/handleGET.ts
+++ src/utils/imagesUtils/handleGET.ts
@@ -2,64 +2,71 @@ import express from 'express';
 import fs from 'fs';
 import sharp from 'sharp';
 
-let resizedImages: {fileName: string, width: number, height:number}[] = [];
+let resizedImages: { fileName: string; width: number; height: number }[] = [];
 
 export let fileFound: boolean = false;
 
 /*
-* responsible to handle the GET request coming from '/images' route
-*/
-const handleRequest = (res: express.Response, fileName: string, width: number, height: number) :void=>{
-
-  resizedImages.find(item => {
-    if (item.fileName === fileName && item.width === width && item.height === height){
+ * responsible to handle the GET request coming from '/images' route
+ */
+export const handleRequest = (res: express.Response, fileName: string, width: number, height: number): void => {
+  resizedImages.find((item) => {
+    if (item.fileName === fileName && item.width === width && item.height === height) {
       fileFound = true;
       showImage(res, fileName);
     }
   });
 
-  if (resizedImages.length === 0 || !fileFound){
-    resizeImage(fileName,width,height).then(()=>
-      showImage(res, fileName));
+  if (resizedImages.length === 0 || !fileFound) {
+    resizeImage(fileName, width, height).then(() => showImage(res, fileName));
   }
 };
 
 /*
-* This function was built up to resize the image
-*/
-const resizeImage = async (fileName:string, width: number, height: number) => {
+ * This function was built up to resize the image
+ */
+export const resizeImage = async (fileName: string, width: number, height: number) : Promise<any> => {
   try {
-    await sharp(`./images/${fileName}.jpg`)
+    const sharpInst = await sharp(`./images/${fileName}.jpg`)
       .resize({
         width: width,
-        height: height
+        height: height,
       })
       .toFile(`./image-resized/${fileName}.jpg`);
-    resizedImages.push({fileName, width, height});
+    resizedImages.push({ fileName, width, height });
+      const metadata = await sharp(`./image-resized/${fileName}.jpg`).metadata()
+      console.log(metadata.width)
+    return metadata
+
   } catch (error) {
     console.log(error);
   }
+
+  return {}
 };
 
 /*
-* This function is responsible for showing the image after retrieving and reading it from which folder it was saved in
-*/
-const showImage = (res: express.Response, fileName: string) => {
+ * This function is responsible for showing the image after retrieving and reading it from which folder it was saved in
+ */
+const showImage = (res: express.Response, fileName: string) :void => {
   fs.readFile(`./image-resized/${fileName}.jpg`, function (err, content) {
     if (err) {
       // the input name is valid but does not exist
-      if(fileName.length !== 0){
-        res.header({'Content-type':'text/html'});
+      if (fileName.length !== 0) {
+        res.header({ 'Content-type': 'text/html' });
         res.status(404);
         console.log(err);
         res.end('404 NOT FOUND');
       }
     } else {
-      res.header({'Content-type':'image/jpg'});
+      res.header({ 'Content-type': 'image/jpg' });
       res.status(200);
       res.end(content);
     }
   });
 };
 
-export default handleRequest;
+module.exports={
+  resizeImage,
+  handleRequest
+};
