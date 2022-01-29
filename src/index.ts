039abfbda3ea2f89307d32e57882f95b2fc4174d
OmarMo7 <omar7.first@gmail.com>
2022-01-29 14:51:04 +0200

    check on missing query values or existent files and then imported the whole file to be used as a middlware.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import router from './routes/images';
+import checkValidation from './utils/imagesUtils/checkValidation';
 
 export const app = express();
 app.use(express.json());
+app.use('/api', checkValidation); // Applying middleware
 app.use('/api', router);
 const port = 3000;
 
-app.listen(port, (()=>{
+app.listen(port, () => {
   console.log(`The server has been se up on port ${port}`);
-}));
+});
 
 module.exports = {
-  app
+  app,
 };
---
+++ src/utils/imagesUtils/checkValidation.ts
@@ -0,0 +1,47 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { handleRequest } from './handleGET';
+
+const checkFileExist = (filename: string): boolean => {
+  const imagePath = path.resolve('./images', filename + '.jpg');
+
+  try {
+    if (fs.existsSync(imagePath)) {
+      return true;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  return false;
+};
+
+const checkValidation = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+  const fileName = req.query.filename as string;
+  const width = parseInt(req.query.width as string);
+  const height = parseInt(req.query.height as string);
+
+  // File name is not provided in the query
+  if (!fileName) {
+    res.status(422).send('File name is missing');
+  }
+
+  // Either width or height or both values are not provided
+  else if (isNaN(width) || isNaN(height)) {
+    res.status(422).send('Invalid input for width or height');
+  }
+
+  // Checking if the file name provided exists
+  else if (!checkFileExist(fileName)) {
+    res.status(404).send('File name not found');
+  }
+
+  // Otherwise, handle the request safely
+  else {
+    handleRequest(res, fileName, width, height);
+  }
+
+  next();
+};
+
+export default checkValidation
